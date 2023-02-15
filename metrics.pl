8482ffda5f0a3906a9c357d071c05c9f62e0b500
Robyn Marowitz <robyn.marowitz@colorado.edu>
2023-02-15 20:17:43 +0000

    bring in country codes file and reference it in metrics script

--- metrics.pl
+++ metrics.pl
@@ -1,22 +1,31 @@
 #!/usr/bin/perl
+use File::Basename;
+use lib dirname(__FILE__);
+use country_codes;
+use strict;
+use Data::Dumper;
 
 #Read in the first ten lines of the file
-@list_from_file = `tail download.log`;
+my @list_from_file = `tail /share/logs/noaa-web/download.log`;
 
-@datasets = ('G02135', 'nsidc-0008', 'nsidc-0057', 'G00788');
-%dataset_sizes;
-%dataset_downloads;
-%unique_users;
+my @datasets = ('G02135', 'nsidc-0008', 'nsidc-0057', 'G00788', 'G00799');
+my %dataset_sizes;
+my %dataset_downloads;
+my %unique_users;
+my %ip_info;
+my %domain_info;
 
-$total_size = 0;
-$total_downloads = 0;
+my $total_size = 0;
+my $total_downloads = 0;
+
+my %country_codes = %country_codes::COUNTRY_CODES;
 
 #Iterate over the lines from the list
-foreach $line_from_file (@list_from_file) {
+foreach my $line_from_file (@list_from_file) {
         #Run our regular expression to parse out fields based on whitespace.
-        @fields_from_line = $line_from_file =~ /(\S+)\s+/g;
+        my @fields_from_line = $line_from_file =~ /(\S+)\s+/g;
 
-        foreach $dataset (@datasets) {
+        foreach my $dataset (@datasets) {
                 if ($fields_from_line[5] =~ /$dataset/) {
                         $dataset_sizes{$dataset} += $fields_from_line[4];
                         $dataset_downloads{$dataset}++;
@@ -28,20 +37,29 @@ foreach $line_from_file (@list_from_file) {
 
         $total_size += $fields_from_line[4];
         $total_downloads++;
+
+        if (! exists $ip_info{$fields_from_line[3]}) {
+          $ip_info{$fields_from_line[3]} = join("", `nslookup $fields_from_line[3]`);
+          $ip_info{$fields_from_line[3]} =~ s/\s//sg;
+        }
+        (my $country_code) = $ip_info{$fields_from_line[3]} =~ /\.([^\.]+)\.$/s;
+        $domain_info{$country_codes{$country_code}}{users}{$fields_from_line[3]} = 1;
+        $domain_info{$country_codes{$country_code}}{downloads}++;
+        $domain_info{$country_codes{$country_code}}{volume}+= $fields_from_line[4];
 }
+
 
 print "Transfers by Data Set\n";
 print "--------------------------------------------------\n";
 print "Dataset, Distinct Users, # of Files, Volume (MB) \n";
 print "--------------------------------------------------\n";
-foreach $dataset (@datasets) {
-        print "$dataset, ,$dataset_sizes{$dataset}, $dataset_downloads{$dataset}\n";
+foreach my $dataset (@datasets) {
+        print "$dataset,    ", scalar(keys %{$unique_users{$dataset}}),", $dataset_downloads{$dataset}, $dataset_sizes{$dataset}\n";
 }
-print keys %unique_users;
 print "\n";
-print values %unique_users;
-print "\n";
-print scalar keys %unique_users;
+foreach my $country_code (keys %domain_info) {
+  print "$country_code, ", scalar(keys %{$domain_info{$country_code}{users}}), "\n"; 
+}
 print "\n";
 print "total size $total_size\n";
 print "total downloads $total_downloads\n";
