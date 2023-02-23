3fc768e18018cd6c138e341779fc9684797d3523
Robyn Marowitz <robyn.marowitz@colorado.edu>
2023-02-23 00:55:59 +0000

    WIP lines_to_raw_fields

--- ingest_logs.py
+++ ingest_logs.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import datetime as dt
 
 # import country_codes
 from misc import RawLogFields, ProcessedLogFields
@@ -20,7 +21,15 @@ def get_log_lines() -> list[str]:
 
 def lines_to_raw_fields(log_lines: list[str]) -> list[RawLogFields]:
     """Convert log lines into self describing data structures."""
-    ...
+    split_line = log_lines[0].split()
+    date = dt.datetime.strptime(split_line[0].strip('['), '%d/%b/%Y:%H:%M:%S'),
+    RawLogfields: RawLogFields = {
+        date : date,
+        ip_address : split_line[3],
+        download_size : int(split_line[4]),
+        file_path : split_line[4]
+    }
+
 
 def process_raw_fields(log_dicts_raw: list[RawLogFields]) -> list[ProcessedLogFields]:
     """Enrich raw log data to include relevant information."""
@@ -36,6 +45,7 @@ def main():
     ...
     log_lines = get_log_lines()
     log_dicts_raw = lines_to_raw_fields(log_lines)
+    breakpoint()
     log_dicts = process_raw_fields(log_dicts_raw)
     write_log_dicts_to_file(log_dicts)
 
