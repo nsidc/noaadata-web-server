d26e61b29c424bde37b9798142e5887cdc79c3fe
Robyn Marowitz <robyn.marowitz@colorado.edu>
2023-03-06 23:24:44 +0000

    Create aggregated dataframes

--- noaa_metrics/aggregate_logs.py
+++ noaa_metrics/aggregate_logs.py
@@ -15,7 +15,10 @@ def downloads_by_dataset(log_df: pd.DataFrame) -> pd.DataFrame:
 
     Counting distinct users, summing total volume, couting number of files.
     """
-    by_dataset_df = ...
+    by_dataset_df = log_df.groupby('dataset').agg(
+        {'ip_address': ['nunique'], 'file_path': ['count'], 'download_bytes': ['sum']})
+    by_dataset_df.columns.set_levels(['Unique Users', 'Files Sent','Download Volume (MB)'], level=1,inplace=True)
+    by_dataset_df.columns = by_dataset_df.columns.droplevel(0)
     return by_dataset_df
 
 
@@ -24,7 +27,12 @@ def downloads_by_day(log_df: pd.DataFrame) -> pd.DataFrame:
 
     Counting distinct users, summing total volume, couting number of files.
     """
-    ...
+    by_day_df = log_df.groupby('date').agg(
+        {'ip_address': ['nunique'], 'file_path': ['count'], 'download_bytes': ['sum']})
+    by_day_df.columns.set_levels(['Unique Users', 'Files Sent','Download Volume (MB)'], level=1,inplace=True)
+    by_day_df.columns = by_day_df.columns.droplevel(0)
+    by_day_df.index = by_day_df.index.rename('Date')
+    return by_day_df
 
 
 def downloads_by_tld(log_df: pd.DataFrame) -> pd.DataFrame:
@@ -33,6 +41,11 @@ def downloads_by_tld(log_df: pd.DataFrame) -> pd.DataFrame:
     Counting distinct users, summing total volume, couting number of files.
     """
     ...
+    by_location_df = log_df.groupby('ip_location').agg(
+        {'ip_address': ['nunique'], 'file_path': ['count'], 'download_bytes': ['sum']})
+    by_location_df.columns.set_levels(['Unique Users', 'Files Sent','Download Volume (MB)'], level=1,inplace=True)
+    by_location_df.columns = by_location_df.columns.droplevel(0)
+    return by_location_df
 
 
 def dataframe_as_text(by_dataset_df):
@@ -46,11 +59,11 @@ def email_full_report(full_report):
 def main():
 
     log_df = create_dataframe(JSON_OUTPUT_FILEPATH)
-    breakpoint()
 
     by_dataset_df = downloads_by_dataset(log_df)
     by_day_df = downloads_by_day(log_df)
     by_location_df = downloads_by_tld(log_df)
+    breakpoint()
 
     by_dataset_text = dataframe_as_text(by_dataset_df)
     by_day_text = dataframe_as_text(...)
