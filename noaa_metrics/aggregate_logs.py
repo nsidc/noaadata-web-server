cef61cc0e0942d743cf607415657228cd63b1089
Robyn Marowitz <robyn.marowitz@colorado.edu>
2023-03-07 01:05:20 +0000

    Update index names and add todo's

--- noaa_metrics/aggregate_logs.py
+++ noaa_metrics/aggregate_logs.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-from noaa_metrics.constants.paths import JSON_OUTPUT_FILEPATH
+from noaa_metrics.constants.paths import JSON_OUTPUT_FILEPATH, REPORT_OUTPUT_DIR
 from noaa_metrics.misc import ProcessedLogFields
 
 
@@ -10,6 +10,7 @@ def create_dataframe(JSON_OUTPUT_FILEPATH) -> pd.DataFrame:
     return log_df
 
 
+# TODO: add totals at the end of each dataframe
 def downloads_by_dataset(log_df: pd.DataFrame) -> pd.DataFrame:
     """Group log_df by dataset.
 
@@ -19,6 +20,7 @@ def downloads_by_dataset(log_df: pd.DataFrame) -> pd.DataFrame:
         {'ip_address': ['nunique'], 'file_path': ['count'], 'download_bytes': ['sum']})
     by_dataset_df.columns.set_levels(['Unique Users', 'Files Sent','Download Volume (MB)'], level=1,inplace=True)
     by_dataset_df.columns = by_dataset_df.columns.droplevel(0)
+    by_dataset_df.index = by_dataset_df.index.rename('Dataset')
     return by_dataset_df
 
 
@@ -46,11 +48,13 @@ def downloads_by_tld(log_df: pd.DataFrame) -> pd.DataFrame:
         {'ip_address': ['nunique'], 'file_path': ['count'], 'download_bytes': ['sum']})
     by_location_df.columns.set_levels(['Unique Users', 'Files Sent','Download Volume (MB)'], level=1,inplace=True)
     by_location_df.columns = by_location_df.columns.droplevel(0)
+    by_location_df.index = by_location_df.index.rename('Domain Type')
     return by_location_df
 
 
-def dataframe_as_text(by_dataset_df):
-    ...
+# TODO: figure out how to format the template.
+def dataframe_as_text(df: pd.DataFrame):
+    df.to_csv(REPORT_OUTPUT_DIR/ 'test.txt')
 
 
 def email_full_report(full_report):
@@ -64,12 +68,12 @@ def main():
     by_dataset_df = downloads_by_dataset(log_df)
     by_day_df = downloads_by_day(log_df)
     by_location_df = downloads_by_tld(log_df)
-    breakpoint()
 
     by_dataset_text = dataframe_as_text(by_dataset_df)
-    by_day_text = dataframe_as_text(...)
-    by_tld_text = dataframe_as_text(...)
+    by_day_text = dataframe_as_text(by_day_df)
+    by_tld_text = dataframe_as_text(by_location_df)
     full_report = f"{by_dataset_text}\n\n{by_day_text}\n\n{by_tld_text}"
+    breakpoint()
     email_full_report(full_report)
 
 
--- noaa_metrics/constants/paths.py
+++ noaa_metrics/constants/paths.py
@@ -4,3 +4,4 @@ PACKAGE_DIR = Path(__file__).parent.parent
 PROJECT_DIR = PACKAGE_DIR.parent
 JSON_OUTPUT_DIR = Path("/tmp")
 JSON_OUTPUT_FILEPATH = JSON_OUTPUT_DIR / "test.json"
+REPORT_OUTPUT_DIR = Path("/tmp")
