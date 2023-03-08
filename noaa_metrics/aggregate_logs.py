18a4ea26c1994dbb3c66293a62f71d0259bc964f
Robyn Marowitz <robyn.marowitz@colorado.edu>
2023-03-08 22:16:45 +0000

    Format with black

--- noaa_metrics/aggregate_logs.py
+++ noaa_metrics/aggregate_logs.py
@@ -10,18 +10,25 @@ def create_dataframe(JSON_OUTPUT_FILEPATH) -> pd.DataFrame:
     return log_df
 
 
-# TODO: add totals at the end of each dataframe
+def get_period_summary_starts(log_df: pd.DataFrame):
+    """Collect stats for entire period."""
+    ...
+
+
 def downloads_by_dataset(log_df: pd.DataFrame) -> pd.DataFrame:
     """Group log_df by dataset.
 
     Counting distinct users, summing total volume, couting number of files.
     """
-    by_dataset_df = log_df.groupby('dataset').agg(
-        {'ip_address': ['nunique'], 'file_path': ['count'], 'download_bytes': ['sum']})
-    by_dataset_df.columns.set_levels(['Distinct Users', 'Files Sent','Download Volume (MB)'], level=1,inplace=True)
+    by_dataset_df = log_df.groupby("dataset").agg(
+        {"ip_address": ["nunique"], "file_path": ["count"], "download_bytes": ["sum"]}
+    )
+    by_dataset_df.columns.set_levels(
+        ["Distinct Users", "Files Sent", "Download Volume (MB)"], level=1, inplace=True
+    )
     by_dataset_df.columns = by_dataset_df.columns.droplevel(0)
-    by_dataset_df.index = by_dataset_df.index.rename('Dataset')
-    by_dataset_df.loc['Total'] = by_dataset_df.sum()
+    by_dataset_df.index = by_dataset_df.index.rename("Dataset")
+    by_dataset_df.loc["Total"] = by_dataset_df.sum()
     return by_dataset_df
 
 
@@ -30,13 +37,16 @@ def downloads_by_day(log_df: pd.DataFrame) -> pd.DataFrame:
 
     Counting distinct users, summing total volume, couting number of files.
     """
-    log_df['date'] = log_df['date'].dt.strftime("%d %b %Y")
-    by_day_df = log_df.groupby('date').agg(
-        {'ip_address': ['nunique'], 'file_path': ['count'], 'download_bytes': ['sum']})
-    by_day_df.columns.set_levels(['Distinct Users', 'Files Sent','Download Volume (MB)'], level=1,inplace=True)
+    log_df["date"] = log_df["date"].dt.strftime("%d %b %Y")
+    by_day_df = log_df.groupby("date").agg(
+        {"ip_address": ["nunique"], "file_path": ["count"], "download_bytes": ["sum"]}
+    )
+    by_day_df.columns.set_levels(
+        ["Distinct Users", "Files Sent", "Download Volume (MB)"], level=1, inplace=True
+    )
     by_day_df.columns = by_day_df.columns.droplevel(0)
-    by_day_df.index = by_day_df.index.rename('Date')
-    by_day_df.loc['Total'] = by_day_df.sum()
+    by_day_df.index = by_day_df.index.rename("Date")
+    by_day_df.loc["Total"] = by_day_df.sum()
     return by_day_df
 
 
@@ -46,18 +56,21 @@ def downloads_by_tld(log_df: pd.DataFrame) -> pd.DataFrame:
     Counting distinct users, summing total volume, couting number of files.
     """
     ...
-    by_location_df = log_df.groupby('ip_location').agg(
-        {'ip_address': ['nunique'], 'file_path': ['count'], 'download_bytes': ['sum']})
-    by_location_df.columns.set_levels(['Distinct Users', 'Files Sent','Download Volume (MB)'], level=1,inplace=True)
+    by_location_df = log_df.groupby("ip_location").agg(
+        {"ip_address": ["nunique"], "file_path": ["count"], "download_bytes": ["sum"]}
+    )
+    by_location_df.columns.set_levels(
+        ["Distinct Users", "Files Sent", "Download Volume (MB)"], level=1, inplace=True
+    )
     by_location_df.columns = by_location_df.columns.droplevel(0)
-    by_location_df.index = by_location_df.index.rename('Domain Type')
-    by_location_df.loc['Total'] = by_location_df.sum()
+    by_location_df.index = by_location_df.index.rename("Domain Type")
+    by_location_df.loc["Total"] = by_location_df.sum()
     return by_location_df
 
 
 # TODO: figure out how to format the template.
 def dataframe_as_text(df: pd.DataFrame):
-    df.to_csv(REPORT_OUTPUT_DIR/ 'test.txt')
+    df.to_csv(REPORT_OUTPUT_DIR / "test.txt")
 
 
 def email_full_report(full_report):
