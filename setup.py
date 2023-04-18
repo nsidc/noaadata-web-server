caa592cc6abe9bdfc44f96e32659019c72ba825d
Robyn Marowitz <robyn.marowitz@colorado.edu>
2023-04-18 17:59:05 +0000

    Bumpversion config

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="noaadata-web-server",
+    version="version="0.1.1",",
+    description="Hosts NSIDC NOAA datasets.",
+    url="git@github.com:nsidc/noaadata-web-server.git",
+    author="National Snow and Ice Data Center",
+    author_email="nsidc@nsidc.org",
+)
